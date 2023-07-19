ce2b5770e93c8f62f03f0c25f22451811af2b195
Suraj Iyer <114157491+surajiyer26@users.noreply.github.com>
2023-07-20 00:08:45 +0530

    Update settings.py

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure--a#u(dbuehz@k-p$77264px!)&wh+e0n9*_vun8=8bb5&6p7f1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
